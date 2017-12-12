6fd037d6d249ff61642c2240539d41437592e59b
cyramic <pughmds@cyram.org>
2017-12-12 17:29:29 +0000

    Chapter list shouldn't be hard-coded.  Not working yet, but small steps...

--- src/lib/chapterlist/chapterlist.js
+++ src/lib/chapterlist/chapterlist.js
@@ -2,16 +2,35 @@ import React, { Component } from 'react';
 import '../../App.css';
 
 class ChapterList extends Component {
+	constructor() {
+		super();
+		this.state = {}
+	}
 
-  render() {
-    return (
-      <select>
-      	<option>--Select One--</option>
-      	<option>Chapter 1</option>
-      	<option>Chapter 2</option>
-      </select>
-    );
-  }
+
+	componentDidMount() {
+		// Won't work as I haven't connected the document id to the state yet...
+		fetch('http://localhost:3000/chapter-list/'+props.documentid)
+			.then((response) => {
+				return response.json()
+			}).then((jsondata) => {
+				this.setState({
+					data : jsondata
+				});
+			}).catch((err) => {
+	  			return err;
+	  		});
+	}
+  
+  	render() {
+    	return (
+      	<select>
+      		<option>--Select One--</option>
+      		<option>Chapter 1</option>
+      		<option>Chapter 2</option>
+      	</select>
+    	);
+  	}
 }
 
 export default ChapterList;
